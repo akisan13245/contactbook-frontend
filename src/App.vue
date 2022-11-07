691431b7e5435d500963702736f48ca6657db740
akisan13245 <akisan993@gmail.com>
2022-11-07 10:11:38 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,22 @@ export default {
   <h1>Hello, Vue.js!</h1>
 </template>
 
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <div class="container mt-3">
+    <router-view />
+  </div>
+</template>
 <style>
 .page {
   max-width: 400px;
